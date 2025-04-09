f25dcd5c497b02c613a6d762acbbe6cd5ea53fbc
Victor Batista <vbatista.dev@gmail.com>
2025-04-09 10:52:20 -0300

    Adicionando dados iniciais do cliente

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                
+                Olá bem-vindo ao Seu Banco
+                
+                Para acessar sua conta informe o seu CPF :D
+                """);
+        String nome = "João de Souza";
+        String cpf = scanner.nextLine();
+        String tipoConta = "Corrente";
+        double valor = 2500.00;
+
+        if(cpf.length() == 11){
+
+            /// Informações iniciais do cliente
+            System.out.println("""
+                    
+                    Olá João, muito bom ter o senhor de volta conosco! 
+                    
+                    ------------------------------
+                    Dados atuais do Cliente:
+                    
+                    Nome:             %s
+                    CFP:              %s
+                    Tipo de conta:    %s
+                    Saldo em conta:   %.2f
+                    ------------------------------
+                    """.formatted(nome, cpf, tipoConta, valor));
+
+        }else {
+            System.out.println("O CPF informado está incorreto");
+        }
     }
 }
